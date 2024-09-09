b0417793d19df61343fa73e4bdddd498b7942ce6
bettercallmahin <bettercallmahin@gmail.com>
2024-09-09 17:59:44 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    var popup = document.querySelector('.cart-popup');
+    popup.classList.add('active');
+}
+
+function hidePopup() {
+    var popup = document.querySelector('.cart-popup');
+    popup.classList.remove('active');
+}
